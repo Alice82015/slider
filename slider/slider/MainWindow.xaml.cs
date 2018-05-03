5b41c14d650dc293cc7fe056d68d77b49209ca2a
m305-9\w401 <a106082015@mail.shu.edu.tw>
2018-05-03 18:07:26 +0800

    first 課堂練習

---
+++ slider/slider/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace slider
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HeightSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // 數值
+            double Value = Math.Round(HeightSlider.Value,2);
+            HeightNumber.Text = Value.ToString();
+
+            // 位置
+            double v = (Value / 200) * 320;
+            Canvas.SetLeft(Height, v);
+
+            // BMI
+            double h = double.Parse(HeightNumber.Text);
+            double w = double.Parse(HeightNumber.Text);
+            double bmi = w / Math.Pow((h / 100), 2);
+            
+
+            // 分割
+            string[] parts = Math.Round(bmi, 1).ToString().Split('.');
+
+            BmiNumber1.Text = parts[0];
+            if (parts.Length > 1)
+                BmiNumber2.Text = "." + parts[1];
+            else
+                BmiNumber2.Text = ".0";
+
+        }
+    }
+}
